50b6548d9ec19716c003494b05408c9f1420f541
D3SOX <nico@d3sox.me>
2023-03-27 21:58:25 +0200

    feat: add timestamps to user model

--- prisma/migrations/20230318151805_add_id_to_spotify_user/migration.sql
+++ prisma/migrations/20230318151805_add_id_to_spotify_user/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[spotifyId]` on the table `SpotifyUser` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `spotifyId` to the `SpotifyUser` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "SpotifyUser" ADD COLUMN     "spotifyId" TEXT NOT NULL;
 
