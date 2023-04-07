30897e7442f6b5312df59e6f0bf00da50494ddfc
D3SOX <nico@d3sox.me>
2023-04-07 22:08:36 +0200

    feat: add playlist name in title on playlist page

--- src/pages/playlist/[id].tsx
+++ src/pages/playlist/[id].tsx
@@ -230,223 +230,228 @@ function Playlist() {
   const isPremium = false;
 
   return (
-    <Stack>
-      <Group grow>
-        <Button type="submit" leftIcon={<IconArchive />} disabled={!isPremium || !selectedMaps.length}>
-          Download as archive (.zip)
-        </Button>
-        <Button
-          type="submit"
-          onClick={handleCreatePlaylist}
-          leftIcon={<IconListDetails />}
-          disabled={!selectedMaps.length}
-        >
-          Download as playlist (.bplist)
-        </Button>
-      </Group>
-      <Group position="center" noWrap>
-        {noMapsFound ? (
-          <Alert icon={<IconAlertCircle />} color="orange">
-            Unfortunately, no maps were found for any of the tracks in this playlist.
-          </Alert>
-        ) : (
-          <>
-            <Text>Selected maps</Text>
-            <SegmentedControl
-              value={noneChecked ? 'none' : allChecked ? 'all' : everyFirstChecked ? 'first' : 'custom'}
-              onChange={type => {
-                if (type === 'none') {
-                  selectNoMaps();
-                } else if (type === 'first') {
-                  selectFirstMap();
-                } else if (type === 'all') {
-                  selectAllMaps();
+    <>
+      <PageTitle title={`Playlist "${data.details.name}"`} />
+      <Stack>
+        <Group grow>
+          <Button type="submit" leftIcon={<IconArchive />} disabled={!isPremium || !selectedMaps.length}>
+            Download as archive (.zip)
+          </Button>
+          <Button
+            type="submit"
+            onClick={handleCreatePlaylist}
+            leftIcon={<IconListDetails />}
+            disabled={!selectedMaps.length}
+          >
+            Download as playlist (.bplist)
+          </Button>
+        </Group>
+        <Group position="center" noWrap>
+          {noMapsFound ? (
+            <Alert icon={<IconAlertCircle />} color="orange">
+              Unfortunately, no maps were found for any of the tracks in this playlist.
+            </Alert>
+          ) : (
+            <>
+              <Text>Selected maps</Text>
+              <SegmentedControl
+                value={
+                  noneChecked ? 'none' : allChecked ? 'all' : everyFirstChecked ? 'first' : 'custom'
                 }
-              }}
-              data={[
-                {
-                  label: (
-                    <Center>
-                      <IconSquare size={16} />
-                      <Box ml={10}>None</Box>
-                    </Center>
-                  ),
-                  value: 'none',
-                },
-                {
-                  label: (
-                    <Center>
-                      <IconSquareCheck size={16} />
-                      <Box ml={10}>First</Box>
-                    </Center>
-                  ),
-                  value: 'first',
-                  disabled: noTrackWithMultipleMaps,
-                },
-                {
-                  label: (
-                    <Center>
-                      <IconChecks size={16} />
-                      <Box ml={10}>All</Box>
-                    </Center>
-                  ),
-                  value: 'all',
-                },
-                {
-                  label: (
-                    <Center>
-                      <IconChecklist size={16} />
-                      <Box ml={10}>Custom</Box>
-                    </Center>
-                  ),
-                  value: 'custom',
-                  disabled: true,
-                },
-              ]}
-            />
-          </>
-        )}
-      </Group>
-      <ScrollArea sx={{ height: 'calc(100vh - 210px)', width: '100%' }} type="auto">
-        <Table>
-          <thead className={classes.header}>
-            <tr>
-              <th>Track</th>
-              <th>Artist</th>
-              <th>Map(s)</th>
-            </tr>
-          </thead>
-          <tbody>
-            {matchedTracks.map(({ track, maps }) => (
-              <tr key={track.id}>
-                <td>
-                  <Group>
-                    {track.album.images && track.album.images.length ? (
-                      <NextImage
-                        src={track.album.images[0].url}
-                        alt="Track image"
-                        width={38}
-                        height={38}
-                      />
-                    ) : (
-                      <Avatar alt="Track image">
-                        <IconMusic />
-                      </Avatar>
-                    )}
-                    <Text>{track.name}</Text>
-                  </Group>
-                </td>
-                <td>
-                  <Group>
-                    {track.artists.map(artist => (
-                      <Anchor key={artist.id} href={artist.uri}>
-                        <Group>
-                          <Avatar alt="Artist image">
-                            <IconUser />
-                          </Avatar>
-                          <Text>{artist.name}</Text>
-                        </Group>
-                      </Anchor>
-                    ))}
-                  </Group>
-                </td>
-                <td>
-                  {maps.length ? (
-                    <Spoiler maxHeight={100} showLabel="Show more" hideLabel="Hide">
-                      <Stack>
-                        {maps.map(map => (
-                          <Group key={map.id} spacing="xs">
-                            <Checkbox
-                              value={map.id}
-                              label={map.name}
-                              checked={selectedMaps.includes(map.id)}
-                              onChange={() => {
-                                if (selectedMaps.includes(map.id)) {
-                                  handlers.filter(id => id !== map.id);
-                                } else {
-                                  handlers.append(map.id);
-                                }
-                              }}
-                            />
-                            <HoverCard
-                              key={map.id}
-                              width={350}
-                              shadow="md"
-                              withArrow
-                              position="top-start"
-                              // openDelay={200}
-                              withinPortal
-                            >
-                              <HoverCard.Target>
-                                <IconInfoCircle size={16} />
-                              </HoverCard.Target>
-                              <HoverCard.Dropdown>
-                                <Stack spacing={1}>
-                                  {map.versions && map.versions.length > 0 && (
-                                    <Group spacing="sm" noWrap>
-                                      <Text weight="bold">Difficulties</Text>
-                                      <Group spacing={5}>
-                                        {map.versions[0].diffs.map(diff => (
-                                          <Badge key={diff.difficulty} size="sm">
-                                            {diff.difficulty}
-                                          </Badge>
-                                        ))}
-                                      </Group>
-                                    </Group>
-                                  )}
-                                  {map.description && (
-                                    <Group spacing={1}>
-                                      <Text weight="bold">Description</Text>
-                                      <Spoiler maxHeight={50} showLabel="Show more" hideLabel="Hide">
-                                        <Text>{map.description}</Text>
-                                      </Spoiler>
-                                    </Group>
-                                  )}
-                                  <Group spacing="sm">
-                                    <Text weight="bold">Ranked</Text>
-                                    {map.ranked ? (
-                                      <IconCheck color="green" size={16} />
-                                    ) : (
-                                      <IconX color="red" size={16} />
-                                    )}
-                                  </Group>
-                                  <Group spacing="sm">
-                                    <Text weight="bold">Automapper</Text>
-                                    {map.automapper ? (
-                                      <IconCheck color="green" size={16} />
-                                    ) : (
-                                      <IconX color="red" size={16} />
-                                    )}
-                                  </Group>
-                                  {map.tags && map.tags.length > 0 && (
-                                    <Group spacing="sm" noWrap>
-                                      <Text weight="bold">Tags</Text>
-                                      <Group spacing={5}>
-                                        {map.tags.map(tag => (
-                                          <Badge key={tag} size="xs">
-                                            {tag}
-                                          </Badge>
-                                        ))}
-                                      </Group>
-                                    </Group>
-                                  )}
-                                </Stack>
-                              </HoverCard.Dropdown>
-                            </HoverCard>
-                          </Group>
-                        ))}
-                      </Stack>
-                    </Spoiler>
-                  ) : (
-                    <Text>No maps found for this track</Text>
-                  )}
-                </td>
+                onChange={type => {
+                  if (type === 'none') {
+                    selectNoMaps();
+                  } else if (type === 'first') {
+                    selectFirstMap();
+                  } else if (type === 'all') {
+                    selectAllMaps();
+                  }
+                }}
+                data={[
+                  {
+                    label: (
+                      <Center>
+                        <IconSquare size={16} />
+                        <Box ml={10}>None</Box>
+                      </Center>
+                    ),
+                    value: 'none',
+                  },
+                  {
+                    label: (
+                      <Center>
+                        <IconSquareCheck size={16} />
+                        <Box ml={10}>First</Box>
+                      </Center>
+                    ),
+                    value: 'first',
+                    disabled: noTrackWithMultipleMaps,
+                  },
+                  {
+                    label: (
+                      <Center>
+                        <IconChecks size={16} />
+                        <Box ml={10}>All</Box>
+                      </Center>
+                    ),
+                    value: 'all',
+                  },
+                  {
+                    label: (
+                      <Center>
+                        <IconChecklist size={16} />
+                        <Box ml={10}>Custom</Box>
+                      </Center>
+                    ),
+                    value: 'custom',
+                    disabled: true,
+                  },
+                ]}
+              />
+            </>
+          )}
+        </Group>
+        <ScrollArea sx={{ height: 'calc(100vh - 210px)', width: '100%' }} type="auto">
+          <Table>
+            <thead className={classes.header}>
+              <tr>
+                <th>Track</th>
+                <th>Artist</th>
+                <th>Map(s)</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </ScrollArea>
-    </Stack>
+            </thead>
+            <tbody>
+              {matchedTracks.map(({ track, maps }) => (
+                <tr key={track.id}>
+                  <td>
+                    <Group>
+                      {track.album.images && track.album.images.length ? (
+                        <NextImage
+                          src={track.album.images[0].url}
+                          alt="Track image"
+                          width={38}
+                          height={38}
+                        />
+                      ) : (
+                        <Avatar alt="Track image">
+                          <IconMusic />
+                        </Avatar>
+                      )}
+                      <Text>{track.name}</Text>
+                    </Group>
+                  </td>
+                  <td>
+                    <Group>
+                      {track.artists.map(artist => (
+                        <Anchor key={artist.id} href={artist.uri}>
+                          <Group>
+                            <Avatar alt="Artist image">
+                              <IconUser />
+                            </Avatar>
+                            <Text>{artist.name}</Text>
+                          </Group>
+                        </Anchor>
+                      ))}
+                    </Group>
+                  </td>
+                  <td>
+                    {maps.length ? (
+                      <Spoiler maxHeight={100} showLabel="Show more" hideLabel="Hide">
+                        <Stack>
+                          {maps.map(map => (
+                            <Group key={map.id} spacing="xs" noWrap>
+                              <Checkbox
+                                value={map.id}
+                                label={map.name}
+                                checked={selectedMaps.includes(map.id)}
+                                onChange={() => {
+                                  if (selectedMaps.includes(map.id)) {
+                                    handlers.filter(id => id !== map.id);
+                                  } else {
+                                    handlers.append(map.id);
+                                  }
+                                }}
+                              />
+                              <HoverCard
+                                key={map.id}
+                                width={350}
+                                shadow="md"
+                                withArrow
+                                position="top-start"
+                                // openDelay={200}
+                                withinPortal
+                              >
+                                <HoverCard.Target>
+                                  <IconInfoCircle size={16} />
+                                </HoverCard.Target>
+                                <HoverCard.Dropdown>
+                                  <Stack spacing={1}>
+                                    {map.versions && map.versions.length > 0 && (
+                                      <Group spacing="sm" noWrap>
+                                        <Text weight="bold">Difficulties</Text>
+                                        <Group spacing={5}>
+                                          {map.versions[0].diffs.map(diff => (
+                                            <Badge key={diff.difficulty} size="sm">
+                                              {diff.difficulty}
+                                            </Badge>
+                                          ))}
+                                        </Group>
+                                      </Group>
+                                    )}
+                                    {map.description && (
+                                      <Group spacing={1}>
+                                        <Text weight="bold">Description</Text>
+                                        <Spoiler maxHeight={50} showLabel="Show more" hideLabel="Hide">
+                                          <Text>{map.description}</Text>
+                                        </Spoiler>
+                                      </Group>
+                                    )}
+                                    <Group spacing="sm">
+                                      <Text weight="bold">Ranked</Text>
+                                      {map.ranked ? (
+                                        <IconCheck color="green" size={16} />
+                                      ) : (
+                                        <IconX color="red" size={16} />
+                                      )}
+                                    </Group>
+                                    <Group spacing="sm">
+                                      <Text weight="bold">Automapper</Text>
+                                      {map.automapper ? (
+                                        <IconCheck color="green" size={16} />
+                                      ) : (
+                                        <IconX color="red" size={16} />
+                                      )}
+                                    </Group>
+                                    {map.tags && map.tags.length > 0 && (
+                                      <Group spacing="sm" noWrap>
+                                        <Text weight="bold">Tags</Text>
+                                        <Group spacing={5}>
+                                          {map.tags.map(tag => (
+                                            <Badge key={tag} size="xs">
+                                              {tag}
+                                            </Badge>
+                                          ))}
+                                        </Group>
+                                      </Group>
+                                    )}
+                                  </Stack>
+                                </HoverCard.Dropdown>
+                              </HoverCard>
+                            </Group>
+                          ))}
+                        </Stack>
+                      </Spoiler>
+                    ) : (
+                      <Text>No maps found for this track</Text>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </ScrollArea>
+      </Stack>
+    </>
   );
 }
 
