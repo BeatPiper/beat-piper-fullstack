ab30d128038f73fc850a59023b988c9c1a62a91b
D3SOX <nico@d3sox.me>
2023-04-07 21:24:37 +0200

    feat: implement account deletion

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,15 +1,38 @@
-import { Button, Card, Group, Stack, Text, TextInput, Title, useMantineTheme } from '@mantine/core';
-import { useSession } from 'next-auth/react';
-import { IconTrash } from '@tabler/icons-react';
+import {
+  Button,
+  Card,
+  Group,
+  Modal,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+  useMantineTheme,
+} from '@mantine/core';
+import { signOut, useSession } from 'next-auth/react';
+import { IconCircleX, IconTrash } from '@tabler/icons-react';
 import PageTitle from '@/components/PageTitle';
+import { trpc } from '@/utils/trpc';
+import { useDisclosure } from '@mantine/hooks';
+import { useRouter } from 'next/router';
 
 function ProfilePage() {
   const { data: session, status } = useSession();
   const theme = useMantineTheme();
+  const router = useRouter();
+
+  const [opened, { open, close }] = useDisclosure(false);
+  const deleteAccountMutation = trpc.deleteUser.useMutation();
 
   if (status !== 'authenticated') {
     return null;
   }
+
+  const deleteAccount = async () => {
+    await deleteAccountMutation.mutateAsync();
+    await signOut({ redirect: false });
+    await router.push('/');
+  };
 
   return (
     <>
@@ -22,10 +45,39 @@ function ProfilePage() {
               <Text color={theme.colors.gray[5]}>Manage your account here</Text>
             </Stack>
 
+            <Modal
+              opened={opened}
+              onClose={close}
+              title="Delete account"
+              centered
+              overlayProps={{
+                color: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
+                opacity: 0.55,
+                blur: 3,
+              }}
+            >
+              <Stack spacing="sm">
+                <Text>Are you sure you want to delete your account?</Text>
+                <Group spacing="sm" grow>
+                  <Button onClick={close} leftIcon={<IconCircleX />}>
+                    Cancel
+                  </Button>
+                  <Button
+                    color="red"
+                    loading={deleteAccountMutation.isLoading}
+                    leftIcon={<IconTrash />}
+                    onClick={() => deleteAccount()}
+                  >
+                    Delete
+                  </Button>
+                </Group>
+              </Stack>
+            </Modal>
+
             <Stack spacing="sm">
               <TextInput label="Name" value={session.user.name ?? ''} readOnly />
               <TextInput label="Email" value={session.user.email ?? ''} disabled />
-              <Button variant="outline" color="red" fullWidth leftIcon={<IconTrash />}>
+              <Button variant="outline" color="red" fullWidth onClick={open} leftIcon={<IconTrash />}>
                 Delete my account
               </Button>
             </Stack>
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,8 +1,16 @@
-import { router } from '../trpc';
+import { protectedProcedure, router } from '../trpc';
 import { spotifyRouter } from '@/server/routers/spotify';
 
 export const appRouter = router({
   spotify: spotifyRouter,
+  deleteUser: protectedProcedure.mutation(async ({ ctx: { session, prisma } }) => {
+    await prisma.user.delete({
+      where: {
+        id: session.user.userId,
+      },
+    });
+    return true;
+  }),
 });
 
 export type AppRouter = typeof appRouter;
